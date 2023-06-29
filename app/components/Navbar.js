22e679611fb689005d0e5c9885ac955e60041dad
gersonja@gmail.com <gersonja@gmail.com>
2023-06-29 11:59:37 -0500

    Actualización página...

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -2,13 +2,13 @@ import Image from "next/image";
 
 export default function Navbar () {
   return (
-    <div className="flex items-center fixed top- w-full px-3 md:px-10 justify-between overflow-hidden bg-green-200">
-      <Image className="p-2 md:p-5 justify-start" src="/logo.svg" width={200} height={50} alt="logo" />
-      <nav className="hidden md:flex font-bold text-green-900">
-        <a className="hover:text-white hover:bg-green-500 px-10 py-2 rounded-xl transition-all duration-500" href="#inicio">Inicio</a>
-        <a className="hover:text-white hover:bg-green-500 px-10 py-2 rounded-xl transition-all duration-500" href="#nosotros">Nosotros</a>
-        <a className="hover:text-white hover:bg-green-500 px-10 py-2 rounded-xl transition-all duration-500" href="#servicios">Servicios</a>
-        <a className="hover:text-white hover:bg-green-500 px-10 py-2 rounded-xl transition-all duration-500" href="#contacto">Contacto</a>
+    <div className="flex items-center fixed top- w-full px-3 md:px-10 justify-between overflow-hidden bg-[#51B4A0]">
+      <Image className="p-2 md:p-5 justify- shadow-white" src="/logo+.svg" width={75} height={75} alt="logo" />
+      <nav className="hidden md:flex font-bold text-[#4C4645]">
+        <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#inicio">Inicio</a>
+        <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#nosotros">Nosotros</a>
+        <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#servicios">Servicios</a>
+        <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#contacto">Contacto</a>
       </nav>
       <div className="flex md:hidden justify-end">
         <button className="m-3">
