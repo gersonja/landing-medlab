e291b4f3c598613f9146868c2e9d09fd2bbea037
gersonja@gmail.com <gersonja@gmail.com>
2023-07-01 08:38:09 -0500

    Menú movil...

--- app/components/Navbar.js
+++ app/components/Navbar.js
@@ -1,20 +1,41 @@
+'use client'
+
 import Image from "next/image";
+import { useState } from "react";
+import { Bars3Icon, XMarkIcon } from "@heroicons/react/24/solid";
+
 
 export default function Navbar () {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
-    <div className="flex items-center fixed top- w-full px-3 md:px-10 justify-between overflow-hidden bg-[#51B4A0]">
-      <Image className="p-2 md:p-5 justify- shadow-white" src="/logo-w.svg" width={200} height={75} alt="logo" />
-      <nav className="hidden md:flex font-bold text-[#4C4645]">
-        <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#inicio">Inicio</a>
-        <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#nosotros">Nosotros</a>
-        <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#servicios">Servicios</a>
-        <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#contacto">Contacto</a>
-      </nav>
-      <div className="flex md:hidden justify-end">
-        <button className="m-3">
-          <svg className="w-6 h-6" fill="none" stroke="green" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h16m-7 6h7" /></svg>
-        </button>
+    <>
+      <div className="flex z-40 items-center fixed top-0 w-full px-3 md:px-10 justify-between overflow-hidden bg-[#51B4A0]">
+        <Image className="p-2 md:p-5 justify- shadow-white" src="/logo-w.svg" width={200} height={75} alt="logo" />
+        <nav className="hidden md:flex font-bold text-[#4C4645]">
+          <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#inicio" onClick={() => setIsOpen(false)}>Inicio</a>
+          <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#nosotros">Nosotros</a>
+          <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#servicios">Servicios</a>
+          <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#contacto">Contacto</a>
+        </nav>
+        <div className="flex md:hidden justify-end">
+          <button className="m-3 transition-all duration-300" onClick={() => setIsOpen(!isOpen)}>
+            {isOpen ? (
+              <XMarkIcon className="h-6 w-6" aria-hidden="true" />
+            ) : (
+              <Bars3Icon className="h-6 w-6" aria-hidden="true" />
+            )}
+          </button>
+        </div>
       </div>
-    </div>
+      {isOpen && (
+        <div className="flex flex-col fixed top-12 right-0 items-center justify-center text-center bg-[#51B4A0] z-50">
+          <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#inicio">Inicio</a>
+          <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#nosotros">Nosotros</a>
+          <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#servicios">Servicios</a>
+          <a className="hover:text-[#51B4A0] hover:bg-white px-10 py-2 rounded-xl transition-all duration-500" href="#contacto">Contacto</a>
+        </div>
+      )}
+    </>
   )
 }
--- app/page.js
+++ app/page.js
@@ -7,7 +7,7 @@ import Link from 'next/link';
 export default function Home () {
   return (
     <>
-      <div className="fixed bottom-3 right-3 md:bottom-10 md:right-10 z-50 hover:animate-pulse cursor-pointer">
+      <div className="fixed bottom-3 right-3 md:bottom-10 md:right-10 z-40 hover:animate-pulse cursor-pointer">
         <Link href="https://api.whatsapp.com/send?phone=593992903801" target="_blank" rel="noopener noreferrer">
           <div className="flex">
             <p className="bg-white p-3 rounded-full">Contáctanos ahora!</p>
